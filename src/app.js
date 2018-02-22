be952a43279af0ac2d2e7ca638cc7a380902f626
lac <laclys@126.com>
2018-02-22 22:14:40 +0800

    added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 const http = require('http')
 const chalk = require('chalk')
+const path =require('path')
+const fs = require('fs')
 const conf = require('./config/defaultConfig')
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello Luke!')
+  const filePath = path.join(conf.root, req.url)
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/plain')
+      res.end(`${filePath} is not a dir or file`)
+      return
+    }
+
+    if (stats.isFile()) {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/plain')
+      // fs.readFile(filePath, (err, data) => {
+      //   res.end(data)
+      // })
+      fs.createReadStream(filePath).pipe(res)  // 用流的方式进行读写
+    } else if (stats.isDirectory()) {
+      fs.readdir(filePath, (err, files) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(files.join(','))
+      })
+    }
+
+  })
 })
 
 server.listen(conf.port, conf.hostname, () => {
