6701c6736a0fbd78bf6ab1ef92c91b02a2a0d4e0
lac <laclys@126.com>
2018-02-27 22:33:36 +0800

    add: helper-openUrl

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 const path =require('path')
 const conf = require('./config/defaultConfig')
 const route = require('./helper/route')
+const openUrl = require('./helper/open')
 
 class Server {
   constructor (config) {
@@ -18,6 +19,7 @@ class Server {
     server.listen(this.conf.port, this.conf.hostname, () => {
       const addr = `http://${this.conf.hostname}:${this.conf.port}`
       console.log(`Server started at ${chalk.green(addr)}`)
+      openUrl(addr)
     })
   }
 }
